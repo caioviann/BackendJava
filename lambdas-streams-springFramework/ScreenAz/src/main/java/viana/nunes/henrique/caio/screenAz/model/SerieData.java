3c7cb81b33c709b207a6728dded7e625719b42cb
caio henrique <caiohenriqueviana01@gmail.com>
2026-03-12 12:15:25 -0300

    adding codes java

--- lambdas-streams-springFramework/ScreenAz/src/main/java/viana/nunes/henrique/caio/screenAz/model/SerieData.java
+++ lambdas-streams-springFramework/ScreenAz/src/main/java/viana/nunes/henrique/caio/screenAz/model/SerieData.java
@@ -1,9 +1,11 @@
 package viana.nunes.henrique.caio.screenAz.model;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record SerieData(@JsonAlias("Title") String title,
                         @JsonAlias("totalSeasons") Integer seasons,
                         @JsonAlias("imdbRating") String rating) {
