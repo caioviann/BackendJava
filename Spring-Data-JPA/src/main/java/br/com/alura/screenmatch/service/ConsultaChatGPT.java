6fa14a1710405aa23a5e116066059913fa474905
caio henrique <caiohenriqueviana01@gmail.com>
2026-04-28 09:47:56 -0300

    adding

--- Spring-Data-JPA/src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
+++ Spring-Data-JPA/src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -5,7 +5,7 @@ import com.theokanning.openai.service.OpenAiService;
 
 public class ConsultaChatGPT {
     public static String obterTraducao(String texto) {
-        OpenAiService service = new OpenAiService("");
+        OpenAiService service = new OpenAiService("Sem API KEY");
 
         CompletionRequest requisicao = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")
