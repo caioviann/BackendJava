b5f0aa2a7ab926c4b87efd8af35e987f62421f78
caio henrique <caiohenriqueviana01@gmail.com>
2026-05-10 17:58:08 -0300

    adicionando relacionamentos.

--- Spring-Data-JPA/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ Spring-Data-JPA/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -88,8 +88,8 @@ public class Principal {
             var serieEncontrada = serie.get();
             List<DadosTemporada> temporadas = new ArrayList<>();
 
-            for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
-                var json = consumo.obterDados(ENDERECO + dadosSerie.titulo().replace(" ", "+") + "&season=" + i + API_KEY);
+            for (int i = 1; i <= serieEncontrada.getTotalTemporadas(); i++) {
+                var json = consumo.obterDados(ENDERECO + serieEncontrada.getTitulo().replace(" ", "+") + "&season=" + i + API_KEY);
                 DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
                 temporadas.add(dadosTemporada);
             }
