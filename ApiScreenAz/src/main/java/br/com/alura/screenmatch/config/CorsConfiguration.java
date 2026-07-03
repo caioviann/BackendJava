c355e52f302aad18147c477c624887ef797be0bd
caio henrique <caiohenriqueviana01@gmail.com>
2026-07-03 11:54:15 -0300

    add Cors Configuration

--- ApiScreenAz/src/main/java/br/com/alura/screenmatch/config/CorsConfiguration.java
+++ ApiScreenAz/src/main/java/br/com/alura/screenmatch/config/CorsConfiguration.java
@@ -10,6 +10,6 @@ public class CorsConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:5500")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TARCE", "CONNECT");
     }
 }
