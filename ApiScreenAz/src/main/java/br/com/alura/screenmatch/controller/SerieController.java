8b8eb8ec2a1e9523827d3e3384687bcef7fa5e8b
caio henrique <caiohenriqueviana01@gmail.com>
2026-07-03 11:20:52 -0300

    add DTO

--- ApiScreenAz/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ ApiScreenAz/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,12 +1,13 @@
 package br.com.alura.screenmatch.controller;
 
 import br.com.alura.screenmatch.Repository.SerieRepository;
-import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.dto.SerieDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
@@ -14,7 +15,9 @@ public class SerieController {
     private SerieRepository repositorio;
 
     @GetMapping("/series")
-    public List<Serie> obterSeries(){
-        return repositorio.findAll();
+    public List<SerieDTO> obterSeries(){
+        return repositorio.findAll().stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
     }
 }
