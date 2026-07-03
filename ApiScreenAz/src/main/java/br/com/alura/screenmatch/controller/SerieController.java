165ee64cbf5369571da272fcd2b55f08c4ffc3b9
caio henrique <caiohenriqueviana01@gmail.com>
2026-07-03 12:49:48 -0300

    add Service Architecture

--- ApiScreenAz/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ ApiScreenAz/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -19,4 +19,9 @@ public class SerieController {
     public List<SerieDTO> obterSeries(){
         return servico.obterTodasAsSeries();
     }
+
+    @GetMapping("/series/top5")
+    public List<SerieDTO> obterTop5Series(){
+        return servico.obterTop5Series();
+    }
 }
